685d78bf2d4de345bf2cf8e81c58bb6c1bf20b03
Sujosh <sujoshk@gmail.com>
2020-06-05 14:22:28 +0530

    process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');;
 
 var PORT = process.env.PORT || 3000;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+
 const db = knex({
     client: 'pg',
     connection: {
